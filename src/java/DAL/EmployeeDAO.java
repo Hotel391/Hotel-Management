5020fc85eda94002e52ae44950882bef07c17b75
githubhieukaisa <vuahoarong@gmail.com>
2025-05-31 08:27:45 +0700

    first commit~

---
+++ src/java/DAL/EmployeeDAO.java
@@ -0,0 +1,71 @@
+package DAL;
+
+import Models.Employee;
+import Models.Role;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeDAO {
+
+    private static EmployeeDAO instance;
+    private Connection con;
+
+    public static EmployeeDAO getInstance() {
+        if (instance == null) {
+            instance = new EmployeeDAO();
+        }
+        return instance;
+    }
+
+    private EmployeeDAO() {
+        con = new DBContext().connect;
+    }
+
+    public List<Employee> getAllEmployee() {
+        List<Employee> list = new ArrayList();
+        String sql = "select e.*, r.RoleName from Employee e\n"
+                + "join Role r on r.RoleId=e.RoleId";
+        try (PreparedStatement st = con.prepareStatement(sql); ResultSet rs = st.executeQuery()) {
+            while (rs.next()) {
+                Employee e = new Employee();
+                e.setEmployeeId(rs.getInt("EmployeeId"));
+                e.setUsername(rs.getString("Username"));
+                e.setPassword(rs.getString("Password"));
+                e.setFullName(rs.getString("FullName"));
+                e.setAddress(rs.getString("Address"));
+                e.setPhoneNumber(rs.getString("PhoneNumber"));
+                e.setEmail(rs.getString("Email"));
+                e.setGender(rs.getBoolean("Gender"));
+                e.setCCCD(rs.getString("CCCD"));
+                e.setDateOfBirth(rs.getDate("dateOfBirth"));
+                e.setRegistrationDate(rs.getDate("registrationDate"));
+                e.setActivate(rs.getBoolean("activate"));
+
+                Role r = new Role();
+                r.setRoleId(rs.getInt("RoleId"));
+                r.setRoleName(rs.getString("RoleName"));
+                e.setRole(r);
+                list.add(e);
+            }
+        } catch (SQLException e) {
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        List<Employee> employees = EmployeeDAO.getInstance().getAllEmployee();
+
+        for (Employee e : employees) {
+            System.out.println("ID: " + e.getEmployeeId());
+            System.out.println("Username: " + e.getUsername());
+            System.out.println("Full Name: " + e.getFullName());
+            System.out.println("Email: " + e.getEmail());
+            System.out.println("Role: " + (e.getRole() != null ? e.getRole().getRoleName() : "No Role"));
+            System.out.println("------------");
+        }
+    }
+}

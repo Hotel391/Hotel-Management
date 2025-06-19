57dda4c6d80fa46927146b24b2714f49813d394b
Tuan <he186833nguyenminhtuan@gmail.com>
2025-06-19 10:37:21 +0700

    add paidAmount

--- src/java/models/Booking.java
+++ src/java/models/Booking.java
@@ -6,6 +6,7 @@ public class Booking {
 
     private int bookingId;
     private int totalPrice;
+    private int paidAmount;
     private Date payDay;
     private String status;  
     private Customer customer;
@@ -14,14 +15,25 @@ public class Booking {
     public Booking() {
     }
 
-    public Booking(int bookingId, int totalPrice, Date payDay, String status, Customer customer, PaymentMethod paymentMethod) {
+    public Booking(int bookingId, int totalPrice, int paidAmount, Date payDay, String status, Customer customer, PaymentMethod paymentMethod) {
         this.bookingId = bookingId;
         this.totalPrice = totalPrice;
+        this.paidAmount = paidAmount;
         this.payDay = payDay;
         this.status = status;
         this.customer = customer;
         this.paymentMethod = paymentMethod;
     }
+
+    public int getPaidAmount() {
+        return paidAmount;
+    }
+
+    public void setPaidAmount(int paidAmount) {
+        this.paidAmount = paidAmount;
+    }
+    
+    
 
     public int getBookingId() {
         return bookingId;

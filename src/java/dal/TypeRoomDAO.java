592d71c75539f6d8fc345409b24f1fb9b86be38f
HaiLong2501 <longx3732@gmail.com>
2025-06-25 00:41:52 +0700

    doing fine checkout late

--- src/java/dal/TypeRoomDAO.java
+++ src/java/dal/TypeRoomDAO.java
@@ -98,6 +98,28 @@ public class TypeRoomDAO {
         }
         return null;
     }
+    
+    //get type room by room number
+    
+    public TypeRoom getTypeRoomByRoomNumber(int roomNumber) {
+        String sql = "SELECT tr.TypeId, tr.TypeName, tr.Description, tr.Price FROM TypeRoom tr JOIN Room r ON tr.TypeId = r.TypeId WHERE r.RoomNumber = ?";
+        try (PreparedStatement st = con.prepareStatement(sql)) {
+            st.setInt(1, roomNumber);
+            try (ResultSet rs = st.executeQuery()) {
+                if (rs.next()) {
+                    TypeRoom typeRoom = new TypeRoom();
+                    typeRoom.setTypeId(rs.getInt(1));
+                    typeRoom.setTypeName(rs.getString(2));
+                    typeRoom.setDescription(rs.getString(3));
+                    typeRoom.setPrice(rs.getInt(4));
+                    return typeRoom;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
     public List<String> getAllTypeRoomName() {
         String sql = "select TypeName from TypeRoom";
